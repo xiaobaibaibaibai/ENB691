abb2244134d2f81476f23ac55035bb22c39ce8a1
Wentan <wentan1995@gmail.com>
2019-02-06 21:15:03 -0500

    hw1

--- assignment1.py
+++ assignment1.py
@@ -68,41 +68,58 @@ x_2 = np.arange(50, 100)
 print(x_1)
 print(x_2)
 ##############
-'''
+
 
 y_1 = None
 y_2 = None
 ##################################################################################
 # 6. Create a 4x4 matrix with values ranging from 0 to 15 and store in variable y.
 # Python
-
+y_1 = []
+for i in range(4):
+    temp = []
+    for j in range(4):
+        temp.append(i*4+j)
+    y_1.append(temp)
 # NumPy
 y_2 = np.arange(16).reshape(4,4)
 
 ##############
 print(y_1)
 print(y_2)
+##############
+
+
+tmp_1 = None
+tmp_2 = None
+####################################################################################
+# 7. Create a 5x5 array with 1 on the border and 0 inside and store in variable tmp.
+# Python
+tmp_1 = []
+tmp_1.append([1]*5)
+for i in range(1, 4):
+    temp = []
+    for j in range(0, 5):
+        if j is 0 or j is 4:
+            temp.append(1)
+        else:
+            temp.append(0)
+    tmp_1.append(temp)
+tmp_1.append([1]*5)
+        
+# NumPy
+tmp_2 = np.ones((5, 5), dtype=np.int)
+tmp_2[1:4,1:4] = np.zeros(3, dtype=np.int)
+
+##############
+print(tmp_1)
+print(tmp_2)
 pythonEndTime = time.time()
 numPyEndTime = time.time()
 print('Python time: {0} sec.'.format(pythonEndTime-pythonStartTime))
 print('NumPy  time: {0} sec.'.format(numPyEndTime-numPyStartTime))
 ##############
-
 '''
-tmp_1 = None
-tmp_2 = None
-####################################################################################
-# 7. Create a 5x5 array with 1 on the border and 0 inside amd store in variable tmp.
-# Python
-
-# NumPy
-
-
-##############
-print(tmp_1)
-print(tmp_2)
-##############
-
 
 a_1 = None; a_2 = None
 b_1 = None; b_2 = None
@@ -110,31 +127,48 @@ c_1 = None; c_2 = None
 #############################################################################################
 # 8. Generate a 50x100 array of integer between 0 and 5,000 and store in variable a.
 # Python
-
+a_1 = []
+for i in range(50):
+    temp = []
+    for j in range(100):
+        temp.append(i*100+j)
+    a_1.append(temp)
 # NumPy
-
+a_2 = np.arange(5000).reshape(50,100)
 
 ###############################################################################################
 # 9. Generate a 100x200 array of integer between 0 and 20,000 and store in variable b.
 # Python
-
+b_1 = []
+for i in range(100):
+    temp = []
+    for j in range(200):
+        temp.append(i*200+j)
+    b_1.append(temp)
 # NumPy
-
+b_2 = np.arange(20000).reshape(100,200)
 
 #####################################################################################
 # 10. Multiply matrix a and b together (real matrix product) and store to variable c.
 # Python
-
+c_1 = []
+for i in range(len(a_1)):
+    temp = []
+    for j in range(len(b_1[0])):
+        num = 0
+        for m in range(len(b_1)):
+            num = a_1[i][m] * b_1[m][j] + num
+        temp.append(num)
+    c_1.append(temp)
 # NumPy
-
-
+c_2 = np.matmul(a_2, b_2)
+'''
 d_1 = None; d_2 = None
 ################################################################################
 # 11. Normalize a 3x3 random matrix ((x-min)/(max-min)) and store to variable d.
 # Python
 
 # NumPy
-
 
 ##########
 print(d_1)
@@ -160,14 +194,19 @@ print(d_2)
 print(np.sum(a_1 == a_2))
 print(np.sum(b_1 == b_2))
 ################
-
+'''
 e_1 = None; e_2 = None
 ###################################################################################
 # 14. Transpose matrix c, add 5 to all elements in matrix, and store to variable e.
 # Python
-
+e_1 = []
+for i in range(len(c_1[0])):
+    temp = []
+    for j in range(len(c_1)):
+        temp.append(c_1[j][i])
+    e_1.append(temp)
 # NumPy
-
+e_2 = c_2.transpose()
 
 ##################
 print (np.sum(e_1 == e_2))
@@ -177,6 +216,11 @@ print (np.sum(e_1 == e_2))
 #####################################################################################
 # 15. Reshape matrix e to 1d array, store to variable f, and print shape of f matrix.
 # Python
-
+f_1 = []
+for i in range(len(e_1)):
+    for j in range(len(e_1[0])):
+        f_1.append(e_1[i][j])
+print("({0},)".format(len(f_1)))
 # NumPy
-'''
+f_2 = np.reshape(e_2, e_2.size)
+print(f_2.shape)
--- test.py
+++ test.py
@@ -1,20 +1,61 @@
 import numpy as np
 
+
 a = np.array([
-    [0, 1],
-    [2, 3],
-    [4, 5]
+    [0, 1, 3, 4],
+    [10, 31, 53, 0],
+    [8, 12, 36, 0]
 ])
 b = np.array([
-    [10],
-    [20],
-    [30]
+    [4, 3, 2, 1],
+    [4, 0, 2, 1],
+    [1, 2, 2, 1],
+    [1, 2, 3, 4],
 ])
 
-aa = np.array([1, 2, 3])
-bb = np.array([
-    [1, 1, 2, 2],
-    [2, 2, 2, 2]
-])
+c = np.matmul(a, b)
+# print(c)
 
-print (aa + bb)
+
+a_1 = [
+    [0, 1, 3, 4],
+    [10, 31, 53, 0],
+    [8, 12, 36, 0]
+]
+
+b_1 = [
+    [4, 3, 2, 1],
+    [4, 0, 2, 1],
+    [1, 2, 2, 1],
+    [1, 2, 3, 4],
+]
+
+c_1 = []
+
+for i in range(len(a_1)):
+    temp = []
+    for j in range(len(b_1[0])):
+        num = 0
+        for m in range(len(b_1)):
+            num = a_1[i][m] * b_1[m][j] + num
+        temp.append(num)
+    c_1.append(temp)
+# print(c_1)
+
+
+e_1 = []
+for i in range(len(c_1[0])):
+    temp = []
+    for j in range(len(c_1)):
+        temp.append(c_1[j][i])
+    e_1.append(temp)
+# print(ct)
+
+e_2 = c.transpose()
+
+
+
+print(np.sum(e_2 == e_1))
+
+f_2 = np.reshape(e_2, e_2.size)
+print(f_2.size)
