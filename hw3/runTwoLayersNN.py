61e9d84ca821761df2d828b9547cf7ef758134de
wentanbai <wentan1995@gmail.com>
2019-03-14 21:51:32 -0400

    hw3

---
+++ hw3/runTwoLayersNN.py
@@ -0,0 +1,163 @@
+import os
+import time
+import numpy as np
+
+# Library for plot the output and save to file
+import matplotlib as mpl
+mpl.use('Agg')
+import matplotlib.pyplot as plt
+
+
+# Load the CIFAR10 dataset
+from keras.datasets import cifar10
+baseDir = os.path.dirname(os.path.abspath(__file__)) + '/'
+classesName = ['plane', 'car', 'bird', 'cat', 'deer', 'dog', 'frog', 'horse', 'ship', 'truck']
+(xTrain, yTrain), (xTest, yTest) = cifar10.load_data()
+xVal = xTrain[49000:, :].astype(np.float)
+yVal = np.squeeze(yTrain[49000:, :])
+xTrain = xTrain[:49000, :].astype(np.float)
+yTrain = np.squeeze(yTrain[:49000, :])
+yTest = np.squeeze(yTest)
+xTest = xTest.astype(np.float)
+
+
+# Show dimension for each variable
+print ('Train image shape:    {0}'.format(xTrain.shape))
+print ('Train label shape:    {0}'.format(yTrain.shape))
+print ('Validate image shape: {0}'.format(xVal.shape))
+print ('Validate label shape: {0}'.format(yVal.shape))
+print ('Test image shape:     {0}'.format(xTest.shape))
+print ('Test label shape:     {0}'.format(yTest.shape))
+
+
+# Show some CIFAR10 images
+plt.subplot(221)
+plt.imshow(xTrain[0])
+plt.axis('off')
+plt.title(classesName[yTrain[0]])
+plt.subplot(222)
+plt.imshow(xTrain[1])
+plt.axis('off')
+plt.title(classesName[yTrain[1]])
+plt.subplot(223)
+plt.imshow(xVal[0])
+plt.axis('off')
+plt.title(classesName[yVal[1]])
+plt.subplot(224)
+plt.imshow(xTest[0])
+plt.axis('off')
+plt.title(classesName[yTest[0]])
+plt.savefig(baseDir+'nn0.png')
+plt.clf()
+
+
+# Normalize the data by subtract the mean image
+meanImage = np.mean(xTrain, axis=0)
+xTrain -= meanImage
+xVal -= meanImage
+xTest -= meanImage
+
+
+# Reshape data from channel to rows
+xTrain = np.reshape(xTrain, (xTrain.shape[0], -1))
+xVal = np.reshape(xVal, (xVal.shape[0], -1))
+xTest = np.reshape(xTest, (xTest.shape[0], -1))
+print ('Train image shape after reshape:   {0}'.format(xTrain.shape))
+print ('Val image shape after reshape:     {0}'.format(xVal.shape))
+print ('Test image shape after reshape:    {0}'.format(xTest.shape))
+print ('\n##############################################################################################')
+
+
+######################################################################################################
+#                                       TWO LAYER Neural Network                                     #
+######################################################################################################
+from twoLayersNN import TwoLayersNN
+numClasses = np.max(yTrain) + 1
+hiddenNeurons = 100
+classifier = TwoLayersNN(xTrain.shape[1], hiddenNeurons, numClasses)
+
+
+# Show weight of network before training
+if classifier.params['w1'] is not None:
+    tmpW = classifier.params['w1']
+    tmpW = tmpW.reshape(32, 32, 3, hiddenNeurons)
+    tmpWMin, tmpWMax = np.min(tmpW), np.max(tmpW)
+    for i in range(hiddenNeurons):
+        plt.subplot(10, 10, i+1)
+        plt.gca().axis('off')
+        wPlot = 255.0 * (tmpW[:, :, :, i].squeeze() - tmpWMin) / (tmpWMax - tmpWMin)
+        plt.imshow(wPlot.astype('uint8'))
+    plt.savefig(baseDir+'nn1.png', bbox_inches='tight')
+    plt.clf()
+
+
+# Training classifier
+startTime = time.time()
+classifier.train(xTrain, yTrain, lr=5e-3, reg=5e-3, iterations=1500 ,verbose=True)
+print ('Training time: {0}'.format(time.time() - startTime))
+
+
+# Calculate accuracy (Should get around this)
+# Training acc:   30.01%
+# Validating acc: 29.29%
+# Testing acc:    30.18%
+print ('Training acc:   {0}%'.format(classifier.calAccuracy(xTrain, yTrain)))
+print ('Validating acc: {0}%'.format(classifier.calAccuracy(xVal, yVal)))
+print ('Testing acc:    {0}%'.format(classifier.calAccuracy(xTest, yTest)))
+
+
+# Show weight of network after training
+if classifier.params['w1'] is not None:
+    tmpW = classifier.params['w1']
+    tmpW = tmpW.reshape(32, 32, 3, hiddenNeurons)
+    tmpWMin, tmpWMax = np.min(tmpW), np.max(tmpW)
+    for i in range(hiddenNeurons):
+        plt.subplot(10, 10, i+1)
+        plt.gca().axis('off')
+        wPlot = 255.0 * (tmpW[:, :, :, i].squeeze() - tmpWMin) / (tmpWMax - tmpWMin)
+        plt.imshow(wPlot.astype('uint8'))
+    plt.savefig(baseDir+'nn2.png')
+    plt.clf()
+
+
+bestParameters = [0, 0]
+bestAcc = -1
+bestModel = None
+################################################################################
+# TODO: 10 points                                                              #
+# Tuneup hyper parameters (regularization strength, learning rate)             #
+# by using validation set.                                                     #
+# - Store the best variables (lr, reg) in bestParameters                       #
+# - Store the best model in bestModel                                          #
+# - Store the best accuracy in bestAcc                                         #
+# - Best Model should get validation accuracy above 35%                        #
+################################################################################
+learningRate = [6e-3, 7e-3, 9e-3]
+regularizationStrength = [6e-3, 7e-3, 9e-3]
+
+for lr in learningRate:
+    for reg in regularizationStrength:
+        classifier = TwoLayersNN(xTrain.shape[1], hiddenNeurons, numClasses)
+        classifier.train(xTrain, yTrain, lr, reg, iterations=1500 ,verbose=False)
+        currentAcc = classifier.calAccuracy(xVal, yVal)
+        # compare accuracy and record best parameters
+        if currentAcc > bestAcc:
+            bestAcc = currentAcc
+            bestModel = classifier
+        bestParameters = [lr,reg]
+
+################################################################################
+#                              END OF YOUR CODE                                #
+################################################################################
+print ('Best validation accuracy: {0}'.format(bestAcc))
+
+
+# Predict with best model
+if bestModel is not None:
+    print ('Best Model parameter, lr = {0}, reg = {1}'.format(bestParameters[0], bestParameters[1]))
+    print ('Training acc:   {0}%'.format(bestModel.calAccuracy(xTrain, yTrain)))
+    print ('Validating acc: {0}%'.format(bestModel.calAccuracy(xVal, yVal)))
+    print ('Testing acc:    {0}%'.format(bestModel.calAccuracy(xTest, yTest)))
+######################################################################################################
+#                                END OF TWO LAYER Neural Network                                     #
+######################################################################################################
---
+++ hw3/twoLayersNN.py
@@ -0,0 +1,216 @@
+import numpy as np
+
+
+class TwoLayersNN (object):
+    """" TwoLayersNN classifier """
+
+    def __init__ (self, inputDim, hiddenDim, outputDim):
+        self.params = dict()
+        self.params['w1'] = None
+        self.params['b1'] = None
+        self.params['w2'] = None
+        self.params['b2'] = None
+        #########################################################################
+        # TODO: 20 points                                                       #
+        # - Generate a random NN weight matrix to use to compute loss.          #
+        # - By using dictionary (self.params) to store value                    #
+        #   with standard normal distribution and Standard deviation = 0.0001.  #
+        #########################################################################
+        sd = 0.0001
+        self.params['w1'] = np.random.randn(inputDim, hiddenDim) * sd
+        self.params['b1'] = np.random.randn(hiddenDim) * sd
+        self.params['w2'] = np.random.randn(hiddenDim, outputDim) * sd
+        self.params['b2'] = np.random.randn(outputDim) * sd
+
+        #########################################################################
+        #                       END OF YOUR CODE                                #
+        #########################################################################
+
+    def calLoss (self, x, y, reg):
+        """
+        TwoLayersNN loss function
+        D: Input dimension.
+        C: Number of Classes.
+        N: Number of example.
+
+        Inputs:
+        - x: A numpy array of shape (batchSize, D).
+        - y: A numpy array of shape (N,) where value < C.
+        - reg: (float) regularization strength.
+
+        Returns a tuple of:
+        - loss as single float.
+        - gradient with respect to each parameter (w1, b1, w2, b2)
+        """
+        loss = 0.0
+        grads = dict()
+        grads['w1'] = None
+        grads['b1'] = None
+        grads['w2'] = None
+        grads['b2'] = None
+        #############################################################################
+        # TODO: 40 points                                                           #
+        # - Compute the NN loss and store to loss variable.                         #
+        # - Compute gradient for each parameter and store to grads variable.        #
+        # - Use Leaky RELU Activation at hidden and output neurons                  #
+        # - Use Softmax loss
+        # Note:                                                                     #
+        # - Use L2 regularization                                                   #
+        # Hint:                                                                     #
+        # - Do forward pass and calculate loss value                                #
+        # - Do backward pass and calculate derivatives for each weight and bias     #
+        #############################################################################
+        
+        # add bias to x and calculate Hin
+        x_b = np.hstack([x, np.ones([x.shape[0], 1])])
+        u_b = np.vstack((self.params['w1'], self.params['b1']))
+        Hin = x_b.dot(u_b)
+        # calculate Hout with bias by RELU
+        Hout = np.maximum(0, Hin)
+        Hout_b = np.append(Hout, np.ones([Hout.shape[0], 1]), axis=1)
+        # calculate scores
+        w_b = np.vstack((self.params['w2'], self.params['b2']))
+        s = Hout_b.dot(w_b)
+        # caclulate correction probability
+        exp_s = np.exp(s)
+        sum_x = np.sum(exp_s, axis=1, keepdims=True)
+        prob = exp_s / sum_x
+        prob_correct = prob[np.arange(x.shape[0]), y]
+        # cacluate loss iwht L2 regularization
+        loss = - np.log(prob_correct)
+        loss = np.sum(loss) / x.shape[0]
+        loss += 0.5 * reg * (np.sum(self.params['w1']**2) + np.sum(self.params['w2']**2))
+
+        # Do backward pass
+        ind = np.zeros_like(prob)
+        ind[np.arange(x.shape[0]), y] = 1
+        ds = prob - ind
+        dw = Hout.T.dot(ds) / x.shape[0]
+        # calculate derivatives for w2 and b2 
+        grads['w2'] = dw + 2 * reg * self.params['w2']
+        grads['b2'] = np.sum(ds, axis=0) / x.shape[0]
+        # calculate derivatives for Hout and Hin 
+        dHout = ds.dot(self.params['w2'].T)
+        dHin = np.zeros(Hin.shape)
+        for i in range(len(Hin)):
+            for j in range((len(Hin[i]))):
+                if Hin[i][j] > 0:
+                    dHin[i][j] = dHout[i][j]
+        # calculate derivatives for w2 and b2 
+        grads['w1'] = x.T.dot(dHin) / x.shape[0] + reg * self.params['w1']
+        grads['b1'] = np.sum(dHin, axis=0) / x.shape[0]
+        #############################################################################
+        #                          END OF YOUR CODE                                 #
+        #############################################################################
+
+        return loss, grads
+
+    def train (self, x, y, lr=5e-3, reg=5e-3, iterations=100, batchSize=200, decay=0.95, verbose=False):
+        """
+        Train this linear classifier using stochastic gradient descent.
+        D: Input dimension.
+        C: Number of Classes.
+        N: Number of example.
+
+        Inputs:
+        - x: training data of shape (N, D)
+        - y: output data of shape (N, ) where value < C
+        - lr: (float) learning rate for optimization.
+        - reg: (float) regularization strength.
+        - iter: (integer) total number of iterations.
+        - batchSize: (integer) number of example in each batch running.
+        - verbose: (boolean) Print log of loss and training accuracy.
+
+        Outputs:
+        A list containing the value of the loss function at each training iteration.
+        """
+
+        # Run stochastic gradient descent to optimize W.
+        lossHistory = []
+        for i in range(iterations):
+            xBatch = None
+            yBatch = None
+            #########################################################################
+            # TODO: 10 points                                                       #
+            # - Sample batchSize from training data and save to xBatch and yBatch   #
+            # - After sampling xBatch should have shape (batchSize, D)              #
+            #                  yBatch (batchSize, )                                 #
+            # - Use that sample for gradient decent optimization.                   #
+            # - Update the weights using the gradient and the learning rate.        #
+            #                                                                       #
+            # Hint:                                                                 #
+            # - Use np.random.choice                                                #
+            #########################################################################
+            
+            # creat xBatch and y Batch
+            D = x.shape[0]
+            index = np.random.choice(D, batchSize, replace=True)
+            xBatch = x[index]
+            yBatch = y[index]
+            # calculate and save loss
+            loss, grads = self.calLoss(xBatch, yBatch, reg)
+            lossHistory.append(loss)
+            # update weights and bias
+            self.params['w1'] = self.params['w1'] - lr * grads['w1']
+            self.params['b1'] = self.params['b1'] - lr * grads['b1']
+            self.params['w2'] = self.params['w2'] - lr * grads['w2']
+            self.params['b2'] = self.params['b2'] - lr * grads['b2']
+            #########################################################################
+            #                       END OF YOUR CODE                                #
+            #########################################################################
+            # Decay learning rate
+            lr *= decay
+            # Print loss for every 100 iterations
+            if verbose and i % 100 == 0 and len(lossHistory) is not 0:
+                print ('Loop {0} loss {1}'.format(i, lossHistory[i]))
+
+        return lossHistory
+
+    def predict (self, x,):
+        """
+        Predict the y output.
+
+        Inputs:
+        - x: training data of shape (N, D)
+
+        Returns:
+        - yPred: output data of shape (N, ) where value < C
+        """
+        yPred = np.zeros(x.shape[0])
+        ###########################################################################
+        # TODO: 10 points                                                         #
+        # -  Store the predict output in yPred                                    #
+        ###########################################################################
+        # calculate x to hidden layer
+        x_b = np.hstack([x, np.ones([x.shape[0], 1])])
+        u_b = np.vstack((self.params['w1'], self.params['b1']))
+        Hin = x_b.dot(u_b)
+        # calculate hidden layer to output
+        Hout = np.maximum(0, Hin)
+        Hout_b = np.append(Hout, np.ones([Hout.shape[0], 1]), axis=1)
+        w_b = np.vstack((self.params['w2'], self.params['b2']))
+        s = Hout_b.dot(w_b)
+        yPred = np.argmax(s, axis=1)
+        ###########################################################################
+        #                           END OF YOUR CODE                              #
+        ###########################################################################
+        return yPred
+
+
+    def calAccuracy (self, x, y):
+        acc = 0
+        ###########################################################################
+        # TODO: 10 points                                                         #
+        # -  Calculate accuracy of the predict value and store to acc variable    #
+        ###########################################################################
+        yPred = self.predict(x)
+        comp = np.sum(yPred == y) 
+        acc = (comp / float(x.shape[0])) * 100.0
+
+        ###########################################################################
+        #                           END OF YOUR CODE                              #
+        ###########################################################################
+        return acc
+
+
+
